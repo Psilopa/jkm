961d49a47d858b8e10a2c5386603ad4a14af6687
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2026-01-30 13:44:13 +0200

    Update jkm_post_singlerun.bat
    Python version update to 3.11.5.

--- jkm_post_singlerun.bat
+++ jkm_post_singlerun.bat
@@ -1,4 +1,5 @@
 chcp 65001 
-C:\Python373\python.exe Z:/jkm/jkm_postprocessing_cli.py -c Z:/jkm/setup/jkm_insectline_singlerun.ini 
+C:\Python3115\python.exe Z:/jkm/jkm_postprocessing_cli.py -c Z:/jkm/setup/jkm_insectline_singlerun.ini 
 @echo off
 set /p id="Enter to quit"
+
