abcfd141317b86ca8f7161d9593a08b6906a8738
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2026-01-30 13:44:41 +0200

    Update jkmpost_plantline_droptarget.bat
    Python version update to 3.11.5.

--- jkmpost_plantline_droptarget.bat
+++ jkmpost_plantline_droptarget.bat
@@ -1,5 +1,6 @@
 chcp 65001 
-C:\Python31153\python.exe C:/ImagingData/jkmulticam/jkmpost_cli.py -c C:/ImagingData/jkmulticam/jkm_win_plantline.ini 
+C:\Python3115\python.exe C:/ImagingData/jkmulticam/jkmpost_cli.py -c C:/ImagingData/jkmulticam/jkm_win_plantline.ini 
 @echo off
 set /p id="Enter to quit"
+
 
