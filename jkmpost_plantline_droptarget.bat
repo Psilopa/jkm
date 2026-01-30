5ec5739fa90d93dc17deb288eb9f40c31db808c2
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2026-01-30 15:01:59 +0200

    Update jkmpost_plantline_droptarget.bat
    jkmpost-cly.py to jkm_postprocessing_cli.py

--- jkmpost_plantline_droptarget.bat
+++ jkmpost_plantline_droptarget.bat
@@ -1,7 +1,8 @@
 chcp 65001 
-C:\Python3115\python.exe C:/ImagingData/jkm/jkmpost_cli.py -c C:/ImagingData/jkm/jkm_win_plantline.ini 
+C:\Python3115\python.exe C:/ImagingData/jkm/jkm_postprocessing_cli.py -c C:/ImagingData/jkm/jkm_win_plantline.ini 
 @echo off
 set /p id="Enter to quit"
+
 
 
 
