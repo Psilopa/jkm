c7a6b1195f062542c5b34e864dfe224482543bbe
Jere Kahanpää <54532067+Psilopa@users.noreply.github.com>
2026-01-30 13:44:24 +0200

    Update jkm_post_insectline.bat
    Python version update to 3.11.5.

--- jkm_post_insectline.bat
+++ jkm_post_insectline.bat
@@ -1,4 +1,5 @@
 chcp 65001 
-C:\Python373\python.exe C:/Insect_APPs/jkm/setup/jkm_postprocessing_cli.py -c C:/Insect_APPs/jkm/setup/jkm_insectline.ini 
+C:\Python3115\python.exe C:/Insect_APPs/jkm/setup/jkm_postprocessing_cli.py -c C:/Insect_APPs/jkm/setup/jkm_insectline.ini 
 @echo off
 set /p id="Enter to quit"
+
